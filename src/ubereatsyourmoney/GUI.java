e6d005a5cff3a5fb7b71adb640e198ebf07f2523
kevSweet <sweetkevindan@gmail.com>
2021-04-26 15:16:17 -0700

    Finished UI

--- src/ubereatsyourmoney/GUI.java
+++ src/ubereatsyourmoney/GUI.java
@@ -6,6 +6,8 @@
 package ubereatsyourmoney;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -21,42 +23,88 @@ import javax.swing.JPanel;
  */
 public class GUI implements ActionListener{
     
-    int count = 0;
     private JFrame frame;
-    private JLabel label;
-    private JPanel panel;
-    
+    private ResizeLabelFont total;
+    private JPanel top;
+    private JPanel bottom;
+    private JButton weekly;
+    private JButton monthly;
+    private JButton ytd;
+    private Font buttonfont = new Font("default", Font.PLAIN,24);
+    private Font selectedfont = new Font("default", Font.BOLD, 24);
+       
     public GUI() {
         
-        frame = new JFrame();
-        JButton button = new JButton("Click Me");
-        label = new JLabel("Number of clicks: 0");
+        frame = new JFrame();                 
+        top = new JPanel();       
+        top.setLayout(new BorderLayout());
         
+        //CALL GET DOLLAR FUNC (WEEKLY)
+        total = new ResizeLabelFont("$50.32");
+        top.add(total);
         
-        panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0,1));
-        panel.add(button);
-        panel.add(label);
+        //initialize button panel and buttons
+        Dimension buttondimension = new Dimension(150,50);       
+        bottom = new JPanel();
+        weekly = new JButton("Weekly");
+        weekly.setPreferredSize(buttondimension);
+        weekly.addActionListener(this);
+        weekly.setFont(selectedfont);
+        monthly = new JButton("Monthly");
+        monthly.addActionListener(this);
+        monthly.setPreferredSize(buttondimension);
+        monthly.setFont(buttonfont);
+        ytd = new JButton("YTD");
+        ytd.addActionListener(this);
+        ytd.setPreferredSize(buttondimension);
+        ytd.setFont(buttonfont);
+        bottom.add(weekly);
+        bottom.add(monthly);
+        bottom.add(ytd);
         
-        button.addActionListener(this);
-        
-        frame.add(panel, BorderLayout.CENTER);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.add(top, BorderLayout.CENTER);
+        frame.add(bottom, BorderLayout.PAGE_END);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Our GUI");
+        frame.setTitle("UEYM");
+        frame.setUndecorated(true);
         frame.pack();
         frame.setVisible(true);
         
         
     }
-    public static void main(String[] args){
-        GUI gui = new GUI();
-    }
-    
+   
     @Override
     public void actionPerformed(ActionEvent e){
-        count++;
-        label.setText("Number of clicks: " + count);
+        //highlight button, change total
+        if(e.getSource() == weekly){
+            //CALL GET DOLLAR FUNC (WEEKLY)
+            resetButtonFonts();
+            total.setText("$90.32");
+            total.adaptLabelFont(total);
+            weekly.setFont(selectedfont);
+        }
+        if(e.getSource() == monthly){
+            //CALL GET DOLLAR FUNC (MONTHLY)
+            resetButtonFonts();
+            total.setText("$900.32");
+            total.adaptLabelFont(total);
+            monthly.setFont(selectedfont);
+        }
+        if(e.getSource() == ytd){
+            //CALL GET DOLLAR FUNC (YEARLY)
+            resetButtonFonts();
+            total.setText("$9000.32");
+            total.adaptLabelFont(total);
+            ytd.setFont(selectedfont);
+        }
+    }
+    
+    //resets buttons to unselected mode
+    public void resetButtonFonts(){
+        weekly.setFont(buttonfont);
+        monthly.setFont(buttonfont);
+        ytd.setFont(buttonfont);
     }
     
     
